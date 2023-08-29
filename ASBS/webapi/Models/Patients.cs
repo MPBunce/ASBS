2acf697cb1348ebebefa910210242ae893424816
MPBunce <mattb1299@gmail.com>
2023-08-28 21:29:25 -0400

    controoller

---
+++ ASBS/webapi/Models/Patients.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace webapi.Models
+{
+    public class Patients
+    {
+
+        [JsonProperty("id")]
+        public string PatientId { get; set;}
+
+        [JsonProperty("firstname")]
+        public string FirstName { get; set;}
+
+        [JsonProperty("lastname")]
+        public string LastName { get; set;}
+
+        [JsonProperty("dateofbirth")]
+        public DateOnly DateOfBirth { get; set;}
+
+        [JsonProperty("gender")]
+        public string Gender { get; set;}
+
+        [JsonProperty("phonenumber")]
+        public string PhoneNumber { get; set;}
+
+        [JsonProperty("email")]
+        public string Email { get; set;}
+
+        [JsonProperty("address")]
+        public string Address { get; set;}
+
+    }
+}
