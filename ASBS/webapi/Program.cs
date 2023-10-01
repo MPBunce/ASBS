0f4edfaaab3255c700cf47c40f083ef6b9d79b16
MPBunce <mattb1299@gmail.com>
2023-10-01 14:57:04 -0400

    alter program cs

--- ASBS/webapi/Program.cs
+++ ASBS/webapi/Program.cs
@@ -76,6 +76,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
